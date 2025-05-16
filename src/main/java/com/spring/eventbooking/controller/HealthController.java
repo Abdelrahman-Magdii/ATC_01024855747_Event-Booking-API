c61eeebe1c2d9bd4b54ab87197bf6fa7d3b9e0d0
Abdelrahman-Magdii <abdulrahman403828@fci.bu.edu>
2025-05-16 09:55:33 +0300

    CICD Pipeline Deploy

--- src/main/java/com/spring/eventbooking/controller/HealthController.java
+++ src/main/java/com/spring/eventbooking/controller/HealthController.java
@@ -1,9 +1,11 @@
-﻿package com.spring.eventbooking.controller;
+package com.spring.eventbooking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/admin")
 public class HealthController {
     @GetMapping("/health")
     public String health() {
